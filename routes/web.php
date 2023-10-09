a7abad8edb274d60c54f51db9511d57bd0681ec7
kristfer <kristfernando8@gmail.com>
2023-10-09 13:14:44 +0700

    Materi Route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,36 @@ Route::get('/profile', function(){
     return view('profile');
 });
 
+//Route dengan Parameter (WAJIB)
+Route::get('/mahasiswa/{nama}', function($nama = "Nur"){
+    echo "<h1>Halo nama saya $nama</h1";
+});
+
+//Route tidak dengan Parameter (Tidak WAJIB)
+Route::get('/mahasiswa2/{nama?}', function($nama = "Nur"){
+    echo "<h1>Halo nama saya $nama</h1";
+});
+
+//Route dengan Parameter > 1
+Route::get('/profile/{nama?}/{pekerjaan?}', function($nama = "Nur", $pekerjaan = "Mahasiswa"){
+    echo "<h1>Halo nama saya $nama kerja $pekerjaan</h1";
+});
+
+//Redirect
+
+Route::get('/hubungi', function () {
+    echo "<h1> Hubungi Kami </h1>";
+}) ->name("Call");
+
+Route::get('/Halo', function () {
+    echo "<a href='" .route('Call') . "'>" . route('Call'). "</a>";
+});
+
+Route::prefix('/dosen')->group(function(){
+    Route::get('/jadwal', function(){
+        echo "<h1>Jadwal dosen</h1>";
+    });
+    Route::get('/materi', function(){
+        echo "<h1>Materi Perkuliahan</h1>";
+    });
+});
